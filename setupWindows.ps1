69b5267cab917715acec7a58086dd8ae16b42883
@rima <32933855+arima0714@users.noreply.github.com>
2021-05-27 11:18:21 +0900

    Update setupWindows.ps1
    .NET Coreをインストールする手順を追加した

--- setupWindows.ps1
+++ setupWindows.ps1
@@ -3,9 +3,10 @@ Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 # 再起動する
-# https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi <- インストールする
 # 手動インストールが必要な手順
+# https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi
 # https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe
+# https://download.visualstudio.microsoft.com/download/pr/d30352fe-d4f3-4203-91b9-01a3b66a802e/bb416e6573fa278fec92113abefc58b3/windowsdesktop-runtime-3.1.15-win-x64.exe
 # 再起動する
 wsl --set-default-version 2
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
