f24b426178a549404a9a2e9bf322c26447f83cd4
@rima <32933855+arima0714@users.noreply.github.com>
2021-05-27 10:45:06 +0900

    Update setupWindows.ps1
    誤って改行していない箇所があったので修正した

--- setupWindows.ps1
+++ setupWindows.ps1
@@ -1,10 +1,11 @@
-# 手動インストールが必要な手順
-# https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exeEnable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 # 再起動する
 # https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi <- インストールする
+# 手動インストールが必要な手順
+# https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe
 wsl --set-default-version 2
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
